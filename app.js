053c22bfd4ad0ff9612bb9c91fd886e7b8a01511
ushasree-mangi <ushasree2022@gmail.com>
2024-05-03 18:09:07 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+
+app.use(express.json())
+const dbPath = path.join(__dirname, "sampleDb.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try { 
+    //initializing the database
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    //running server on  3004 port
+    app.listen(3004, () => {
+      console.log("Server Running at http://localhost:3004/");
+    });
+  } catch (e) {
+    //handling exceptions
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+//API 1 - get all posts api
+app.get("/posts/",async(request,response)=>{
+    const {
+    offset = 0,
+    limit = 15,
+    order_by='id',
+    order = "ASC",
+    search_q = "",
+    tag="",
+    }=request.query;
+    console.log(`tag value ${tag}`);
+    
+    const getPostsQuery=`
+    select * from posts WHERE tag LIKE '%${tag}%' or (title LIKE '%${search_q}%' or description LIKE '%${search_q}%')
+    ORDER BY ${order_by} ${order}
+    LIMIT ${limit} OFFSET ${offset};
+    `; 
+    const data=await db.all(getPostsQuery);
+    response.send(data);
+
+})
+
+//API2 - creating a post api
+app.post("/post/",async(request,response)=>{
+    const {id,title,description,tag,imageUrl}=request.body
+    const createPostQuery=`
+    INSERT INTO POSTS(id,title, description , imageUrl, tag) 
+    values('${id}','${title}', '${description}' ,'${imageUrl}','${tag}' );`;
+
+    const dbResponse = await db.run(createPostQuery);
+    const postId = dbResponse.lastID;
+    response.send({ postId: postId });
+})
---
+++ query.sql
@@ -0,0 +1,15 @@
+--create table posts (id int primary key,title varchar,description text,imageUrl text,tag varchar);
+
+/*insert INTO posts(id ,title,description,imageUrl,tag)
+values (6,'Mindfulness Tips','Practice mindfulness throughout the day to reduce stress and increase overall well-being.','https://res.cloudinary.com/dtcogqxhp/image/upload/v1714707571/2148923997_rdh8p0.jpg','Mindfulness'),
+(7,'Healthy Smoothie Recipes','Blend up a delicious and nutritious smoothie packed with fruits, veggies, and superfoods!','https://res.cloudinary.com/dtcogqxhp/image/upload/v1664246353/samples/food/fish-vegetables.jpg','Healthy Eating'),
+(8,'Sunset Yoga','Unwind and stretch with a calming yoga session as you watch the sun dip below the horizon.','https://res.cloudinary.com/dtcogqxhp/image/upload/v1714707888/49_yomr7d.jpg','Yoga'),
+(9,'Self-Care Sunday','Take some time to pamper yourself and recharge for the week ahead.','https://res.cloudinary.com/dtcogqxhp/image/upload/v1714708092/14456931_5466436_dybqvc.jpg','Self-Care'),
+(10,'Travel Bucket List','Dreaming of your next adventure? Start planning your ultimate travel bucket list today!','https://res.cloudinary.com/dtcogqxhp/image/upload/v1714708261/1356_bjpddp.jpg','Travel'),
+(11,'Healthy Snack Ideas','Snack smart with these delicious and nutritious snack ideas that will keep you energized throughout the day.','https://res.cloudinary.com/dtcogqxhp/image/upload/v1714708448/76056_bjgd0n.jpg','Healthy Eating'),
+(12,'Motivational Quotes','Find inspiration and motivation with these uplifting quotes to keep you going when times get tough.','https://res.cloudinary.com/dtcogqxhp/image/upload/v1714708697/120891_i7ojyh.jpg','Motivation'),
+(13,'Sunrise over the Mountains','Witness the breathtaking beauty of the sunrise as it illuminates the majestic mountain peaks.','https://res.cloudinary.com/dtcogqxhp/image/upload/v1714709113/adam-kool-ndN00KmbJ1c-unsplash_yfh2yk.jpg','Nature'),
+(14,'Golden Sunset at the Beach','Experience the magic of a golden sunset casting its warm glow over the sandy shores and crashing waves.','https://res.cloudinary.com/dtcogqxhp/image/upload/v1714709261/frank-mckenna-OD9EOzfSOh0-unsplash_hpouru.jpg','Nature'),
+(15,'Enchanting Forest Path','Embark on a journey through an enchanting forest path adorned with vibrant foliage and dappled sunlight.','https://res.cloudinary.com/dtcogqxhp/image/upload/v1714709387/luca-bravo-ESkw2ayO2As-unsplash_pukyzn.jpg','Nature');
+*/
+select * from posts;
